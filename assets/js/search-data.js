3ec775d346b4b0d807b123cf0c5450582818a52b
dnnxl <51223285+dnnxl@users.noreply.github.com>
2025-08-16 19:34:24 +0000

    Deploying to gh-pages from @ dnnxl/dnnxl.github.io@3dd1d6924b3e90b2d90431a35c5e8a05c55132a1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -24,8 +24,8 @@ ninja.data = [{
             window.location.href = "/publications/";
           },
         },{id: "nav-repositories",
-          title: "repositories",
-          description: "Edit the `_data/repositories.yml` and change the `github_users` and `github_repos` lists to include your own GitHub profile and repositories.",
+          title: "Repositories",
+          description: "Here are some of my repositories.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/repositories/";
@@ -36,13 +36,6 @@ ninja.data = [{
           section: "Navigation",
           handler: () => {
             window.location.href = "/cv/";
-          },
-        },{id: "nav-teaching",
-          title: "teaching",
-          description: "Materials for courses you taught. Replace this text with your description.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/teaching/";
           },
         },{id: "nav-people",
           title: "people",
