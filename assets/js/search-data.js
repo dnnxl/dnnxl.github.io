55caca4d5046453e127602b2b1d39c7ab7bf2e1c
dnnxl <51223285+dnnxl@users.noreply.github.com>
2025-08-29 03:14:24 +0000

    Deploying to gh-pages from @ dnnxl/dnnxl.github.io@1035db95a5fca987ef97bab35b456492917f5818 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -430,8 +430,8 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},{id: "news-at-the-centro-nacional-de-alta-tecnología-cenat-i-served-as-a-course-instructor-for-the-big-data-school-2020-where-i-delivered-the-workshop-machine-learning-methods-and-deep-learning-with-images-teaching-practical-applications-of-these-techniques-with-image-data-and-contributing-to-capacity-building-in-data-science-within-the-national-research-and-education-community",
-          title: 'At the Centro Nacional de Alta Tecnología (CeNAT), I served as a Course...',
+            },},{id: "news-at-centro-nacional-de-alta-tecnología-cenat-i-was-a-course-instructor-for-for-big-data-school-2020-delivering-a-workshop-on-machine-learning-and-deep-learning-with-images-and-helping-build-data-science-capacity-in-the-national-research-community",
+          title: 'At Centro Nacional de Alta Tecnología (CeNAT), I was a Course Instructor for...',
           description: "",
           section: "News",},{id: "news-my-work-segment-anything-model-and-few-shot-models-for-object-detection-has-been-accepted-for-a-poster-presentation-at-the-eastern-european-machine-learning-summer-school-eeml-in-novi-sad-serbia",
           title: 'My work, “Segment Anything Model and Few Shot Models for Object Detection” has...',
