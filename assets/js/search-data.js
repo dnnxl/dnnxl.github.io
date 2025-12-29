ffdc6b9153b7bc7d4e9d62eb527a993cb66b9528
dnnxl <51223285+dnnxl@users.noreply.github.com>
2025-12-29 01:10:36 +0000

    Deploying to gh-pages from @ dnnxl/dnnxl.github.io@f4c8388f27c8faaec020b0f193bdd4615bf5b342 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -44,6 +44,13 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/volunteering/";
           },
+        },{id: "nav-projects",
+          title: "projects",
+          description: "A growing collection of your cool projects.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/projects/";
+          },
         },{id: "nav-cv",
           title: "cv",
           description: "Explore my professional experience, education, skills, and selected projects. You can also download a PDF version using the button above",
@@ -466,7 +473,52 @@ ninja.data = [{
           section: "News",},{id: "news-our-work-has-been-accepted-at-the-7th-ieee-international-conference-on-bioinspired-processing-in-universidad-nacional-campus-pérez-zeledón-costa-rica-titled-learning-compact-representations-of-agricultural-fields-a-study-of-variational-autoencoders-variants-for-aerial-drone-imagery-for-oral-presentation",
           title: 'Our work has been accepted at the 7th IEEE International Conference on BioInspired...',
           description: "",
-          section: "News",},{
+          section: "News",},{id: "projects-project-1",
+          title: 'project 1',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/1_project/";
+            },},{id: "projects-project-2",
+          title: 'project 2',
+          description: "a project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/2_project/";
+            },},{id: "projects-project-3-with-very-long-name",
+          title: 'project 3 with very long name',
+          description: "a project that redirects to another website",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/3_project/";
+            },},{id: "projects-project-4",
+          title: 'project 4',
+          description: "another without an image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/4_project/";
+            },},{id: "projects-project-5",
+          title: 'project 5',
+          description: "a project with a background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/5_project/";
+            },},{id: "projects-project-6",
+          title: 'project 6',
+          description: "a project with no image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/6_project/";
+            },},{id: "projects-project-7",
+          title: 'project 7',
+          description: "with background image",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/7_project/";
+            },},{id: "projects-project-8",
+          title: 'project 8',
+          description: "an other project with a background image and giscus comments",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/8_project/";
+            },},{id: "projects-project-9",
+          title: 'project 9',
+          description: "another project with an image 🎉",
+          section: "Projects",handler: () => {
+              window.location.href = "/projects/9_project/";
+            },},{
         id: 'social-github',
         title: 'GitHub',
         section: 'Socials',
